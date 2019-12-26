9678cdd33dfb9baa6c6a4dd693b6b42e57db764e
protagonisths <13837050616@163.com>
2019-12-26 15:03:00 +0800

    modify package.json

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,26 @@ module.exports = {
         pathRewrite: {
           "^/development": ""
         }
+      },
+      "/prod": {
+        target: "http://production.com/",
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          "^/prod": ""
+        }
       }
+    }
+  },
+
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.optimization.minimizer[0].options.terserOptions.compress.warnings = false;
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true;
+      config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = [
+        "console.log"
+      ];
     }
   }
 };
