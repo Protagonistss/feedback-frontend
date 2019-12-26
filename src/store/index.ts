628b378dad3782238bd794337259ebeae039fcba
protagonisths <13837050616@163.com>
2019-12-26 14:46:11 +0800

    lint style

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import header from "./modules/header";
 import user from "./modules/user";
-import persist from './plugins/persist'
+import persist from "./plugins/persist";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
--- src/store/plugins/persist.ts
+++ src/store/plugins/persist.ts
@@ -1,17 +1,16 @@
 export default (store: any) => {
-    if (localStorage) {
-        let auth: any = localStorage.getItem('auth')
-        const user: any = JSON.parse(auth)
-        if (user) {
-            store.commit("user/login", user)
-        }
+  if (localStorage) {
+    let auth: any = localStorage.getItem("auth");
+    const user: any = JSON.parse(auth);
+    if (user) {
+      store.commit("user/login", user);
     }
+  }
 
-    // subscribe of store about mutation.  like react action.type
-    store.subscribe((mutation: any, state: any) => {
-        if (mutation.type === 'user/login') {
-            localStorage.setItem('auth', JSON.stringify(state.user.isLogin))
-        }
-    })
-
+  // subscribe of store about mutation.  like react action.type
+  store.subscribe((mutation: any, state: any) => {
+    if (mutation.type === "user/login") {
+      localStorage.setItem("auth", JSON.stringify(state.user.isLogin));
+    }
+  });
 };
